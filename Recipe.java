5a6826115f327a3cd44696aa8aaae2b987c7b235
Judyyy <55216250+zl2877@users.noreply.github.com>
2022-09-30 19:31:08 -0400

    Add files via upload
    Class recipe

---
+++ Recipe.java
@@ -0,0 +1,64 @@
+
+import java.util.Comparator;
+
+public class Recipe{
+	
+	
+	String name; //name
+	String description; //description
+	String ingredientList; //Ingredient list
+	String instructions; //instructions
+	
+	//constructor
+	public Recipe(String n, String d, String ingList, String ins) {
+		name = n;
+		description =d;
+		ingredientList = ingList;
+		instructions = ins;
+	}
+	
+	//getters and setters
+	 public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public String getIngredientList() {
+			return ingredientList;
+		}
+
+		public void setIngredientList(String ingredientList) {
+			this.ingredientList = ingredientList;
+		}
+
+		public String getInstructions() {
+			return instructions;
+		}
+
+		public void setInstructions(String instructions) {
+			this.instructions = instructions;
+		}
+
+
+	//return string representation
+	public String toString() {
+		
+		return name+ "\t"+ description + "\t" + ingredientList + "\t" + instructions +"\t";
+	}
+
+
+	 
+	
+
+}
